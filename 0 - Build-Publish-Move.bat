66bed2d21218dbd0a270d02bce9c40979c6efbb2
hc_zhang <hc_zhang@trip.com>
2021-08-19 17:00:58 +0800

    update bat

--- 0 - Build-Publish-Move.bat
+++ 0 - Build-Publish-Move.bat
@@ -5,5 +5,6 @@ dotnet restore
 dotnet build --no-restore -c Release
 
 move /Y Panosen.CodeDom.Java\bin\Release\Panosen.CodeDom.Java.*.nupkg D:\LocalSavoryNuget\
+move /Y Panosen.CodeDom.Java.Engine\bin\Release\Panosen.CodeDom.Java.Engine.*.nupkg D:\LocalSavoryNuget\
 
 pause
